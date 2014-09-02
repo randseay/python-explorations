b5ccf81c822118bf3028b50cf17cc37a0d227893
Rand Seay <rand.seay@ndus.edu>
2014-09-02 18:46:23 -0500

    Initial commit.

---
+++ inclass_lab1.py
@@ -0,0 +1,8 @@
+# comment
+
+fruit1 = "Orange"
+fruit2 = "Grapes"
+fruit3 = "Apples"
+fruit4 = "Cherry"
+
+print(fruit1,fruit2,fruit3,fruit4)
---
+++ randseay_lab1.py
@@ -0,0 +1,12 @@
+# Displays a users name and course information using three print statements
+
+first_name = "Rand"
+last_name = "Seay"
+course_id = "CSci 160"
+course_name = "Computer Science I"
+email = "rand.seay@ndus.edu"
+school_name = "University of North Dakota"
+
+print(first_name,last_name)
+print(course_id,course_name,email)
+print(school_name)
