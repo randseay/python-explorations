ec424102af47141a8fddd0b16d36961d741d23fe
Rand Seay <rand.seay@ndus.edu>
2014-09-16 17:42:16 -0500

    Update the third inclass assignment to contain the second part of the lab.

--- inclass_lab3.py
+++ inclass_lab3.py
@@ -1,12 +1,21 @@
 """
 Author: Rand Seay
-Objective: Calculate the discount of an item based on its value.
+Objective: In-class assignment for the third lab
 """
+
+# Objective: Calculate the discount of an item based on its value.
+
+print("\nWelcome to the discount calculator, where you can find discounts.")
+print("="*80)
 
 while True:
     try:
         itemValue = float(input("Please provide a value for an item: "))
-        break
+
+        if itemValue > 0:
+            break
+        else:
+            print("That is not a positive value! Please try again.")
     except ValueError:
         print("That is not a valid value! Please try again.")
 
@@ -23,3 +32,50 @@ if discount != -1:
     print("Your discount is {:.0%}!".format(discount))
 else:
     print("There is currently no discount for an item of that value. Sorry.")
+
+# Objective: Trucks, Cars, and People
+
+print("\nWelcome to cars, trucks, and people! We can help you get where you are going.")
+print("="*80)
+
+while True:
+    try:
+        trucks = int(input("Trucks hold 5 people. How many trucks are there? "))
+
+        if trucks > 0:
+            break
+        else:
+            print("That is not a positive value! Please try again.")
+    except ValueError:
+        print("Please provide a real amount of trucks.")
+
+while True:
+    try:
+        cars = int(input("Cars hold four people. How many cars are there? "))
+
+        if cars > 0:
+            break
+        else:
+            print("That is not a positive value! Please try again.")
+    except ValueError:
+        print("Please provide a real amount of cars.")
+
+while True:
+    try:
+        people = int(input("How many people are there? "))
+
+        if people > 0 and people <= 30:
+            break
+        else:
+            print("You can't have that many people! Tell some to go home and try again.")
+    except ValueError:
+        print("Please provide a real amount of people.")
+
+print("\n{} cars can hold {} people. {} trucks can hold {} people. You have {} people".format(cars, cars*4, trucks, trucks*5, people))
+
+if cars * 4 >= people:
+    print("Take cars.")
+elif trucks * 5 >= people:
+    print("Take trucks.")
+else:
+    print("It is not possible to get where you want to go with those vehicles. Tough luck.")
