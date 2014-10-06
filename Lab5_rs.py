6cd402a97e2fce3a194c554cee1b8d56061b921c
Rand Seay <rand.seay@ndus.edu>
2014-10-06 11:15:43 -0500

    Finish the fifth lab.

--- Lab5_rs.py
+++ Lab5_rs.py
@@ -13,89 +13,54 @@ def sectionIntro(nameOfSection):
     print("="*80)
     print()
 
-def getNumFromUser(valueType, prompt, positiveValue=False, defaultValue=False):
+def getNumFromUser(valueType, prompt, enforcePositiveValue=False, specifyDefaultValue=False, defaultValue=0):
     """
     Return a numeric value based on passed-in type and prompt
     """
     while True:
         try:
             if valueType == "integer" or valueType == "int":
-                userInput = int(input(prompt + "\n> "))
-                if userInput == "":
-                    if defaultValue:
-                        userInput = int(defaultValue)
-                    else:
-                        raise RuntimeError("Default value is invalid ({}).".format(defaultValue))
-            elif valueType == "float":
-                userInput = float(input(prompt + "\n> "))
-                if userInput == "":
-                    if defaultValue:
-                        userInput = float(defaultValue)
-                    else:
-                        raise RuntimeError("Default value is invalid ({}).".format(defaultValue))
-            else:
-                raise RuntimeError("Invalid valueType parameter, please select 'int' (or 'integer') or 'float'.")
 
-            if positiveValue:
+                userInput = input(prompt + "\n> ")
+
+                if defaultValue and userInput == "":
+                    try:
+                        userInput = int(defaultValue)
+                    except ValueError:
+                        raise RuntimeError("The specified default value ({}) is invalid.".format(defaultValue))
+                else:
+                    try:
+                        userInput = int(userInput)
+                    except (ValueError, TypeError) as e:
+                        print("Unable to cast value ({}) as an integer. Please try again.\n".format(userInput))
+                        continue
+
+            if valueType == "float":
+
+                userInput = input(prompt + "\n> ")
+
+                if defaultValue and userInput == "":
+                    try:
+                        userInput = float(defaultValue)
+                    except ValueError:
+                        raise RuntimeError("The specified default value ({}) is invalid.".format(defaultValue))
+                else:
+                    try:
+                        userInput = float(userInput)
+                    except (ValueError, TypeError) as e:
+                        print("Unable to cast value ({}) as a float. Please try again.\n".format(userInput))
+                        continue
+
+            if enforcePositiveValue:
                 if userInput > 0:
                     break
                 else:
-                    print("Number was specified as positive, but is not positive ({}).\n".format(userInput))
+                    print("Number was specified as positive, but is not positive ({}). Please try again.\n".format(userInput))
             else:
                 break
         except:
             raise RuntimeError("Invalid data type passed to getNumFromUser under the set conditions ({}).".format(valueType))
     return userInput
-
-def getIntFromUser(prompt):
-    """
-    Return an integer based on user input and a passed-in prompt
-    """
-    while True:
-        try:
-            userInput = int(input(prompt + "\n> "))
-        except ValueError:
-            userInput = "Please provide a positive whole number as a value"
-
-        break
-    return userInput
-
-def getPositiveIntFromUser(prompt):
-    """
-    Return a positive integer based on user input and a passed-in prompt
-    """
-    while True:
-        try:
-            userInput = int(input(prompt + "\n> "))
-
-            if userInput > 0:
-                break
-            else:
-                userInput = "Please provide a positive whole number as a value."
-        except ValueError:
-            userInput = "Please provide a positive whole number as a value"
-    return userInput
-
-def requestPositiveIntFromUser(prompt, default):
-    """
-    Optionally return a positive integer based on user input and a passed-in prompt
-    """
-    userInput = int(default)
-    while True:
-        try:
-            userInput = int(input(prompt + "\n> "))
-
-            if userInput > 0:
-                break
-            else:
-                userInput = "Please provide a positive whole number as a value."
-        except ValueError:
-            userInput = "Please provide a positive whole number as a value"
-    return userInput
-
-# getNumFromUser Test
-testNumber = getNumFromUser("int", "Please choose a positive integer.", positiveValue=True, defaultValue=2)
-print("Your test number is {}.".format(testNumber))
 
 # Part 1: Mathematical Calculations
 
@@ -110,7 +75,11 @@ def getNaturalLog(numberOfIterations):
 
     return result
 
-iterationsOfLN = getPositiveIntFromUser("Please provide a positive whole number to determine the number of iterations\nused in the approximation of natural logarithm.")
+iterationsOfLN = getNumFromUser(
+    "int",
+    "Please provide a positive whole number to determine the number of iterations\nused in the approximation of natural logarithm.",
+    enforcePositiveValue=True
+)
 print()
 
 print("The natural logarithm after {} iterations is {}".format(iterationsOfLN, getNaturalLog(iterationsOfLN)))
@@ -132,7 +101,11 @@ def getFibSeries(numberOfIterations):
 
     return fibonacciSeries
 
-iterationsOfFib = getPositiveIntFromUser("Please provide a positive whole number to determine the length of the Fibonacci series.")
+iterationsOfFib = getNumFromUser(
+    "int",
+    "Please provide a positive whole number to determine the length of the Fibonacci series.",
+    enforcePositiveValue=True
+)
 
 print("\nHere is your Fibonacci series with a length of {}\n> ".format(iterationsOfFib), end="")
 for item in getFibSeries(iterationsOfFib):
@@ -147,30 +120,76 @@ def countUp(val1, val2, step=1):
     result = []
 
     if val1 < val2:
-        for value in range(val1, val2, step):
+        for value in range(val1, val2 + 1, step):
             result.append(value)
     else:
-        for value in range(val2, val1, step):
+        for value in range(val2, val1 + 1, step):
             result.append(value)
 
     return result
 
-def countDown(val1, val2, step=1):
+def countDown(val1, val2, step=2):
     result = []
+
+    if val1 < val2:
+        for value in range(val2, val1 - 1, -step):
+            result.append(value)
+    else:
+        for value in range(val1, val2 - 1, -step):
+            result.append(value)
 
     return result
 
-firstValue = getIntFromUser("Please provide an initial value to count to or from.")
-secondValue = getIntFromUser("Please provide another value to count to or from. The smaller number will be used as a starting number.")
-stepValue = requestPositiveIntFromUser("Please indicate what the step should be, press enter to accept the default of 1.", 1)
+# Count up
 
-countUp(firstValue, secondValue, stepValue)
+firstUpValue = getNumFromUser("int", "Please provide an initial whole number to count to or from.")
+print()
+secondUpValue = getNumFromUser("int", "Please provide another whole number to count to or from. The smaller number will be used as a starting number.")
+print()
+stepUpValue = getNumFromUser(
+    "int",
+    "Please indicate what the step should be, press ENTER to accept the default of 1.",
+    enforcePositiveValue=True,
+    specifyDefaultValue=True,
+    defaultValue=1
+)
 
+print("\nHere is your counting up sequence, counting by {}.".format(stepUpValue))
+for item in countUp(firstUpValue, secondUpValue, stepUpValue):
+    print(item, end=" ")
+print()
+
+# Count Down
+
+print()
+firstDownValue = getNumFromUser("int", "Please provide an initial whole number to count down to or down from.")
+print()
+secondDownValue = getNumFromUser("int", "Please provide another whole number to down to count to or down from. The larger number will be used as a starting number.")
+print()
+stepDownValue = getNumFromUser(
+    "int",
+    "Please indicate what the step down should be, press ENTER to accept the default of 2.",
+    enforcePositiveValue=True,
+    specifyDefaultValue=True,
+    defaultValue=2
+)
+
+print("\nHere is your counting down sequence, counting by {}.".format(stepDownValue))
+for item in countDown(firstDownValue, secondDownValue, stepDownValue):
+    print(item, end=" ")
+print()
 
 # Part 4: Calculate the calories burned
 
+sectionIntro("Calories Burned")
 
+def caloriesBurned(minutesExercised):
+    return 3.4 * minutesExercised
 
-# Part 5: Draw a Triangle
+minutesUserExercised = getNumFromUser(
+    "float",
+    "Please provide the amount of time in minutes that you played Wii Sports.",
+    enforcePositiveValue=True
+)
 
-
+print("\nYou played Wii Sports for {:.2F} minutes, and burned {:.2F} calories.".format(minutesUserExercised, caloriesBurned(minutesUserExercised)))
