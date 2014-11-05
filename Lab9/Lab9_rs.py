932d44d299084a3c0c0657de757db2812dce022e
Rand Seay <rand.seay@ndus.edu>
2014-11-04 18:28:21 -0600

    Work on Lab9.

--- Lab9/Lab9_rs.py
+++ Lab9/Lab9_rs.py
@@ -1,4 +1,92 @@
 """
 Author: Rand Seay
-Objective:
+Objective: Working with Dictionaries
 """
+
+def addClass(classes, className, grade):
+    """
+    Adds a class with className to the classes dictionary and return True if it
+    was added or False if it is already in the dictionary
+    """
+
+    if className in classes:
+        return False
+    else:
+        classes[className] = grade
+        return True
+
+def importReportCard(classes, filename="reportcard.txt"):
+    """
+    Reads in a file containing class names and grades, which are separated by a
+    ':' (colon), then the contents of the report card are added to the classes
+    dictionary
+    """
+
+    inputFile = open(filename)
+    for line in inputFile:
+        if ":" in line:
+            splitLine = line.strip().split(":")
+            addClass(classes, splitLine[0], splitLine[1])
+        else:
+            print("Improperly formatt line {}".format(line.strip()))
+    return(classes)
+
+
+def dropClass(classes, className):
+    """
+    Drops (removes) a class with className from the classes dictionary and
+    returns True if it was removed or False if it was not in the dictionary
+    """
+
+def printClasses(classes):
+    """
+    Prints out all of the classes
+    """
+
+def getGPA(classes):
+    """
+    Returns the GPA for the classes passed in as a floating point number
+    """
+
+def editGrade(classes, className, grade):
+    """
+    Edits the grade of a certain class and returns True if it was successful
+    and false if the class is not in the dictionary
+    """
+
+def takenClass(classes, className):
+    """
+    Returns True if the class is in the dictionary and False if it is not
+    """
+
+def printAllGrades(classes):
+    """
+    Prints all the grades (separated by spaces)
+    """
+
+def numberOfGrades(classes):
+    """
+    Returns a new dictionary with the letter grades as keys and number of
+    instances of that grade as a value
+    """
+
+def printClassesWithHigherGrade(classes,grade):
+    """
+    Prints all the classes (in the same format as printClasses) where the grade
+    for the class is equal to or greater then the grade passed in
+    """
+
+def main():
+    """
+    The main function
+    """
+
+    classes = {}
+
+    importReportCard(classes)
+
+    print(classes)
+
+
+
+main()
--- Lab9/inclass_lab9.py
+++ Lab9/inclass_lab9.py
@@ -1,4 +1,104 @@
 """
 Author: Rand Seay
-Objective:
+Objective: ???
 """
+
+"""
+dict1 = {'new1': 1111, 'new2': 2222}
+print(dict1)
+dict1['new3'] = 3333
+print(dict1)
+print(dict([('key1',1111),('key2',2222),('key3',3333),('key4',4444)]))
+print({x: x**2 for x in (2,4,6)})
+print(dict(type1=1111,type2=2222,type3=3333))
+dict1['new4'] = 4444
+print(dict1)
+dict1['new3'] = 5555
+print(dict1)
+dict1[1] = 7777
+print(dict1)
+dict1['a'] = 7777
+print(dict1)
+del dict1['new3']
+print(dict1)
+print(dict1.keys())
+print(dict1.values())
+print('new2' in dict1)
+print(dict1.items())
+
+list = [1,2,3,4,5]
+list.append(x)
+list.insert(i, x)
+print(list[-1])
+insert(len(list), x) #append
+list.remove(x)
+list.pop(x)
+"""
+
+"""
+Create file1 and file2
+In which file you want to write a number?
+Enter number
+Write number into specified file
+At the end close the file
+Open both files again
+Read all number from file and store into list
+Sort the list
+
+Write a program that takes a filename and
+string from user and returns a number of times
+the string is occur in the file(ignore
+special characters and case(upper/lower case) of
+the words)
+
+Give options for the user till it press 'Y' and
+stop asking once user presses 'N'
+
+1. insert
+2. remove
+3. sort
+4. print values
+5. print keys
+6. search whether key is exist or not
+    Enter key
+7. print all items from the list
+"""
+
+while True:
+    whichFile = input("Would you like to write numbers to file1 or file2?\n> ")
+
+    if whichFile == "file1":
+        fileContents = open("file1.txt", "w")
+        break
+    elif whichFile == "file2":
+        fileContents = open("file2.txt", "w")
+        break
+    else:
+        print('\nThat is not a valid file, please enter either "file1" or "file2".\n')
+
+count = 0
+while True and count < 10:
+    try:
+        yourNumber = int(input("Please enter a number to add to the file.\n> "))
+
+        fileContents.write(str(yourNumber) + "\n")
+        count += 1
+        if count >= 10:
+            break
+    except ValueError :
+        print("\nThat is not a valid number, please try again.\n")
+fileContents.close()
+
+file1 = open("file1.txt")
+file1Contents = []
+for line in file1:
+    file1Contents.append(int(line.strip()))
+
+file2 = open("file2.txt")
+file2Contents = []
+for line in file2:
+    file2Contents.append(int(line.strip()))
+
+contentList = file1Contents + file2Contents
+contentList.sort()
+print("Here is the sorted list of the contents of both files.\n>", contentList)
