b648dae26acb10cb555c3a4b3265c713c126d194
Rand Seay <rand.seay@ndus.edu>
2014-11-07 12:09:04 -0600

    Complete assignment 1.

--- Lab9/Lab9_rs.py
+++ Lab9/Lab9_rs.py
@@ -3,6 +3,8 @@ Author: Rand Seay
 Objective: Working with Dictionaries
 """
 
+# Assignment 1
+
 def addClass(classes, className, grade):
     """
     Adds a class with className to the classes dictionary and return True if it
@@ -27,7 +29,7 @@ def importReportCard(classes, filename="reportcard.txt"):
             splitLine = line.strip().split(":")
             addClass(classes, splitLine[0], splitLine[1])
         else:
-            print("Improperly formatted line data in line {} ({})".format(lineNum, line.strip()))
+            print("Improperly formatted data in line {} ({})".format(lineNum, line.strip()))
         lineNum += 1
     return(classes)
 
@@ -53,7 +55,7 @@ def printClasses(classes):
     for key in sortedKeys:
         count += 1
         print("{}. {:8s}: {}".format(count, key, classes[key]))
-    print("The GPA: {}".format(getGPA(classes)))
+    print("The GPA: {:.2F}".format(getGPA(classes)))
 
 def getGPA(classes):
     """
@@ -83,37 +85,100 @@ def editGrade(classes, className, grade):
     Edits the grade of a certain class and returns True if it was successful
     and false if the class is not in the dictionary
     """
+    if className in classes:
+        classes[className] = grade
+        return True
+    else:
+        return False
 
 def takenClass(classes, className):
     """
     Returns True if the class is in the dictionary and False if it is not
     """
+    return True if className in classes else False
 
 def printAllGrades(classes):
     """
     Prints all the grades (separated by spaces)
     """
+    print("All Grades:")
+    for k, v in classes.items():
+        print(v, end = " ")
+    print()
 
 def numberOfGrades(classes):
     """
     Returns a new dictionary with the letter grades as keys and number of
     instances of that grade as a value
     """
+    gradeAccumulator = {}
+    for k, v in classes.items():
+        if v in gradeAccumulator:
+            gradeAccumulator[v] += 1
+        else:
+            gradeAccumulator[v] = 1
 
-def printClassesWithHigherGrade(classes,grade):
+    return gradeAccumulator
+
+def printClassesWithHigherGrade(classes, grade):
     """
     Prints all the classes (in the same format as printClasses) where the grade
     for the class is equal to or greater then the grade passed in
     """
+    sortedKeys = list(classes.keys())
+    sortedKeys.sort()
+    sortedClasses = {}
+    count = 0
+    for key in sortedKeys:
+        if classes[key] <= grade:
+            count += 1
+            print("{}. {:8s}: {}".format(count, key, classes[key]))
+            sortedClasses[key] = classes[key]
+    print("The GPA: {:.2F}".format(getGPA(sortedClasses)))
 
 def main():
     """
     The main function
     """
-
+    # Initialize classes dictionary
     classes = {}
 
+    # Add a class
+    addClass(classes, "ENGR 220", "A")
+
+    # Import report card
     importReportCard(classes)
+
+    # Drop a class
+    dropClass(classes, "MATH 370")
+
+    # Print classes
     printClasses(classes)
+    print("-"*80)
+
+    # Edit grade
+    editGrade(classes, "CSci 160", "B")
+
+    # Check if student has taken a class
+    print("Student has taken CSci 160:", takenClass(classes, "CSci 160"))
+    print("Student has taken CHEM 330:", takenClass(classes, "CHEM 330"))
+    print("-"*80)
+
+    # Print All Grades
+    printAllGrades(classes)
+    print("-"*80)
+
+    # Print number of grades
+    print("Number of grades:\n{}".format(numberOfGrades(classes)))
+    print("-"*80)
+
+    # Print classes with a higher grade
+    gradeToSort = "B"
+    print("Classes with a {} or higher:".format(gradeToSort))
+    printClassesWithHigherGrade(classes, gradeToSort)
 
 main()
+
+# Assignment 2
+
+
