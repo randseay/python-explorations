8ab8efa0adaec2b5b357e1b37e8737cd62f580ec
Rand Seay <rand.seay@gmail.com>
2014-12-03 10:58:27 -0600

    Initialize Lab11.

--- Lab11/Lab11_rs.py
+++ Lab11/Lab11_rs.py
@@ -1,4 +1,84 @@
 """
 Author: Rand Seay
-Objective:
+Objective: Work with dictionaries and lists, and be an awesome gambler.
 """
+import random
+
+# Blackjack Program
+
+faceCardValues = {
+    "jack": 10,
+    "queen": 10,
+    "king": 10,
+    "ace": 11
+}
+
+def makeDeck():
+    """
+    Creates a new deck of cards
+    """
+    suits = ["spades", "hearts", "clubs", "diamonds"]
+    num = list(range(2,11))
+    num.extend(['jack','queen','king','ace'])
+    return [(j,i) for j in num for i in suits]
+
+def dealCards(deck):
+    """
+    Deals cards from a passed in deck and returns a hand of cards
+    """
+    hand = []
+    random.shuffle(deck)
+    hand.append(deck.pop())
+    hand.append(deck.pop())
+    return hand
+
+def getHandValue(hand):
+    """
+    Calculates and returns the value of a passed in hand of cards
+    """
+    value = 0
+    numOfAces = 0
+    for card in hand:
+        if card[0] in faceCardValues:
+            value += int(faceCardValues[card[0]])
+            if card[0] == "ace":
+                numOfAces += 1
+        else:
+            value += int(card[0])
+
+    for card in range(1, numOfAces + 1):
+        if value > 21:
+            value -= 10
+    return value
+
+def getCard(hand, deck):
+    """
+    Gets a card from a passed in deck of cards, and adds it to the passed in
+    hand of cards
+    """
+    hand.append(deck.pop())
+    return hand, deck
+
+def printHand(hand):
+    """
+    Prints a passed in hand of cards
+    """
+    print("Your hand:\n")
+    for card in yourHand:
+        print()
+
+def main():
+    """
+    The main function
+    """
+    deck = makeDeck()
+    yourHand = dealCards(deck)
+    print("Here is your hand:\n{}\nvalue: {}".format(yourHand, getHandValue(yourHand)))
+
+    getCard(yourHand, deck)
+    print(yourHand)
+    print("Here is your hand:\n{}\nvalue: {}".format(yourHand, getHandValue(yourHand)))
+
+    print("The size of the deck is {}.".format(len(deck)))
+
+main()
--- Lab11/inclass_lab11.py
+++ Lab11/inclass_lab11.py
@@ -1,4 +1,58 @@
 """
 Author: Rand Seay
-Objective:
+Objective: Work with dictionaries
 """
+
+# Part 1
+
+def main():
+    wordFile = open("words.txt")
+    wordList = []
+    filteredList = []
+
+    for line in wordFile:
+        wordList.append(line.rstrip())
+    wordFile.close()
+
+    firstLetter = input("Type the first letter you would like to filter (r). Type 'QUIT' to exit.\n> ")
+    if firstLetter != "QUIT":
+        for word in wordList:
+            if word[0] == firstLetter:
+                filteredList.append(word)
+
+    print(filteredList)
+
+    secondLetter = input("Type the second letter you would like to filter. 'QUIT' to exit.\n> ")
+    if secondLetter != "QUIT":
+        for word in filteredList:
+            if word[1] != secondLetter:
+                filteredList.remove(word)
+
+    print(filteredList)
+
+    # Part 2
+
+    codexFile = open("keys.txt")
+    codex = {}
+
+    for line in codexFile:
+        codex[line.rstrip()] = ""
+    codexFile.close()
+
+    keyCount = 0
+    for key in codex.keys():
+        codex[key] = wordList[keyCount]
+        keyCount += 1
+
+    print(codex)
+
+    fileToEncrypt = open("original.txt")
+    linesToEncrypt = []
+
+    for line in fileToEncrypt:
+        linesToEncrypt.append(line.rstrip())
+    fileToEncrypt.close()
+
+    print(linesToEncrypt)
+
+main()
