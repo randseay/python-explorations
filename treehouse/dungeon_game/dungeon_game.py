8781d1eaa281b5b3bbd01c4383192903c6b24c9f
Rand Seay <rand.seay@gmail.com>
2015-02-18 11:53:57 -0600

    Initialize dungeon game.

---
+++ treehouse/dungeon_game/dungeon_game.py
@@ -0,0 +1,22 @@
+"""
+Title: Dungeon Game
+Author: Rand Seay
+"""
+
+import random
+
+def get_locations():
+    """
+    Sets random locations for the player, the monster, and the door. If any of
+    them are at the same location, start over.
+    """
+
+def main():
+    """
+    The game's main function
+    """
+    print("="*80 + "\nWelcome to the Dungeon Game!\n" + "="*80)
+    # while True:
+    dungeon_size = tuple(input("What size would you like your dungeon to be? (x,y)\n> ").split(","))
+
+main()
